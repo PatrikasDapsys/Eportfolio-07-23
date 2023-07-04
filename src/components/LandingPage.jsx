7f224b755030dd90fdff2c2b4af738ab75b1f537
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-07-05 00:12:38 +0300

    setup

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='text-[50px]'> 
+      We promptly judged antique ivory buckles for the next prize.
+    </div>
+  )
+}
+
+export default LandingPage
