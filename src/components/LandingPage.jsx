e5ecd5b572a6bdcc79e24e857251df11e65642ee
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-07-05 17:47:05 +0300

    landing no js

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function LandingPage() {
+  const outsideText =
+    "text-primary-500 whitespace-nowrap absolute tracking-widest";
+
   return (
-    <div className='text-[50px]'> 
-      We promptly judged antique ivory buckles for the next prize.
-    </div>
-  )
+    <section className="w-screen h-screen bg-primary-900 flex justify-center p-[30px] relative">
+      <span
+        className={outsideText + " left-[-45px] top-[80px] rotate-[270deg] "}
+      >
+        Patrikas Dapsys
+      </span>
+      <span
+        className={outsideText + " right-[-45px] bottom-[80px] rotate-[90deg] "}
+      >
+        Patrikas Dapsys
+      </span>
+      <div className="w-full border-2 border-primary-500">
+        <div className="text-white text-3xl p-8 w-[50%]">
+          <div className="mb-3 text-5xl">Patrikas Dapsys</div>
+          <div className="text-xs text-gray-200">
+            I'm a Frontend software engineer with a strong passion for building
+            web applications with great user experiences.
+          </div>
+        </div>
+        <div className="w-[16px] h-6 border rounded-full block absolute bottom-10 left-1/2 transform -translate-x-1/2">
+          <div className="w-1 h-1 border rounded-full absolute top-[5px] left-[5.5px] animate-bouncing"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-900": "#0c0c0c",
+        "primary-300": "#A8A8A8",
+        "primary-500": "#808080",
+      },
+      keyframes: {
+        bouncing: {
+          "0%": {
+            transform:  'translateY(0px)'
+          },
+          '100%': {
+            transform:  'translateY(6px)'
+          }
+        }
+      },
+      animation: {
+        'bouncing': 'bouncing 400ms alternate infinite'
+      }
+    },
   },
   purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   plugins: [],
